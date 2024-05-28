a2f48f3015ffb1d6be987ea4d4bed19d1824b180
CMinder <minderctrl@gmail.com>
2024-05-28 19:43:30 +0800

    1.尝试使用axios在Main组件挂载前向后端要数据 2.处理跨域请求问题

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,13 +9,35 @@
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue'
+import {ref, onBeforeMount} from 'vue'
+import axios from "axios"
 
 const item = {
   date: '2016-05-02',
   name: 'Tom',
   address: 'No. 189, Grove St, Los Angeles',
 }
+const baseURL = "http://localhost:8090/user"
+
+function loadData() {//组件挂载之前从后端获取数据
+  let promise = axios.get(baseURL, {
+    params: {
+      pageSize: 10,
+      pageNum: 1
+    }
+  })
+  promise.then(response => {
+    console.log(response.data)
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+onBeforeMount(() => {
+  loadData()
+})
+
+
 const tableData = ref(Array.from({length: 10}).fill(item))
 </script>
 
