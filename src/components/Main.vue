e4bd2a9eb230ef192735c3418fba9f0d87ea5171
CMinder <minderctrl@gmail.com>
2024-05-28 21:51:47 +0800

    1.重写了Main中表格的列名以对应后端返回的数据 2.使用tag转换列值 3.使用header-cell-tag设置表头样式（注：必须加上v-bind:，只有这样才能将""中的数据转换成对象） 4.加上边框 5.增加了操作按钮

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,40 @@
 <template>
   <el-scrollbar>
-    <el-table :data="tableData">
-      <el-table-column prop="date" label="Date" width="140"/>
-      <el-table-column prop="name" label="Name" width="120"/>
-      <el-table-column prop="address" label="Address"/>
+    <el-table :data="tableData" border stripe
+              :header-cell-style="{background:'#fafafa',textAlign:'center'}">
+      <el-table-column prop="id" label="ID" width="100px"/>
+      <el-table-column prop="username" label="账号" width="150px"/>
+      <el-table-column prop="nickname" label="姓名"/>
+      <el-table-column prop="roleId" label="角色">
+        <template v-slot="scope">
+          <el-tag v-bind:type="getRoleType(scope.row.roleId)" size="large">
+            {{ getRoleMsg(scope.row.roleId) }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="gender" label="性别" width="100px">
+        <template v-slot="scope">
+          <el-tag v-bind:type="getGenderType(scope.row.gender)" size="large">
+            {{ scope.row.gender }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="phone" label="电话"/>
+      <el-table-column prop="operation" label="操作">
+        <el-tag type="primary" size="large">编辑</el-tag>
+        <el-tag type="danger" size="large">删除</el-tag>
+      </el-table-column>
     </el-table>
   </el-scrollbar>
 </template>
 
 <script lang="ts" setup>
-import {ref, onBeforeMount} from 'vue'
+import {onBeforeMount, ref} from 'vue'
 import axios from "axios"
 
-const item = {
-  date: '2016-05-02',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles',
-}
 const baseURL = "http://localhost:8090/user"
+
+let tableData = ref([])
 
 function loadData() {//组件挂载之前从后端获取数据
   let promise = axios.get(baseURL, {
@@ -27,7 +44,13 @@ function loadData() {//组件挂载之前从后端获取数据
     }
   })
   promise.then(response => {
-    console.log(response.data)
+    if (response.data.code == 200) {
+      tableData.value = response.data.data.data
+      console.log(tableData)
+
+    } else {
+      alert("数据获取失败")
+    }
   }).catch(error => {
     console.log(error)
   })
@@ -38,9 +61,38 @@ onBeforeMount(() => {
 })
 
 
-const tableData = ref(Array.from({length: 10}).fill(item))
+function getRoleType(roleId) {//获取角色tag标签的type
+  switch (roleId) {
+    case 0:
+      return "danger"
+    case 1:
+      return "primary"
+    case 2:
+      return "success"
+  }
+}
+
+function getRoleMsg(roleId) {//获取角色tag标签的替换值
+  switch (roleId) {
+    case 0:
+      return "超级管理员"
+    case 1:
+      return "管理员"
+    case 2:
+      return "普通用户"
+  }
+}
+
+function getGenderType(gender) {//获取角色tag标签的type
+  switch (gender) {
+    case "男":
+      return "primary"
+    case "女":
+      return "warning"
+  }
+}
+
 </script>
 
 <style scoped>
-
 </style>
