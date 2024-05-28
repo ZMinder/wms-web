c567aac78b267c63c4a944a3a5be37f002383d26
CMinder <minderctrl@gmail.com>
2024-05-28 11:47:12 +0800

    1.调整了Header组件的布局

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,59 @@
 <template>
   <div class="toolbar">
+    <el-icon class="icon-expand">
+      <Expand/>
+    </el-icon>
+    <div class="welcome-text">
+      <span>欢迎来到仓库管理系统</span>
+    </div>
+    <span class="username">Tom</span>
     <el-dropdown>
-      <el-icon style="margin-right: 8px; margin-top: 1px">
-        <setting/>
+      <el-icon class="drop-icon">
+        <ArrowDownBold/>
       </el-icon>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item>View</el-dropdown-item>
-          <el-dropdown-item>Add</el-dropdown-item>
-          <el-dropdown-item>Delete</el-dropdown-item>
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
-    <span>Tom</span>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {Setting} from '@element-plus/icons-vue'
+import {ArrowDownBold, Expand} from '@element-plus/icons-vue'
 
 </script>
 
 <style scoped>
 .toolbar {
-  display: inline-flex;
+  display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   height: 100%;
-  right: 20px;
+}
+
+.icon-expand {
+  flex-shrink: 0;
+  font-size: 25px;
+}
+
+.welcome-text {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+  align-items: center;
+}
+
+.username {
+  font-size: 15px;
+  margin-right: 5px;
+}
+
+.drop-icon {
+  font-size: 12px;
+  margin-top: 1px;
 }
 </style>
