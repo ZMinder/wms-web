15de1543c97361fb388c4201c29ea23231998e37
CMinder <minderctrl@gmail.com>
2024-05-29 20:18:48 +0800

    1.添加了新增用户的功能，但是缺少了对用户数据的校验

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,6 +9,7 @@
       </el-select>
       <el-button type="primary" @click="fuzzyLoad()">查询</el-button>
       <el-button type="success" @click="resetQuery()">重置</el-button>
+      <el-button type="primary" @click="saveUser()">添加用户</el-button>
     </div>
     <el-table :data="userData.data" border stripe
               :header-cell-style="{background:'#fafafa',textAlign:'center'}"
@@ -16,13 +17,7 @@
       <el-table-column prop="id" label="ID" width="100px"/>
       <el-table-column prop="username" label="账号" width="150px"/>
       <el-table-column prop="nickname" label="姓名"/>
-      <el-table-column prop="roleId" label="角色">
-        <template v-slot="scope">
-          <el-tag v-bind:type="getRoleType(scope.row.roleId)" size="large">
-            {{ getRoleMsg(scope.row.roleId) }}
-          </el-tag>
-        </template>
-      </el-table-column>
+      <el-table-column prop="age" label="年龄" width="100px"/>
       <el-table-column prop="gender" label="性别" width="100px">
         <template v-slot="scope">
           <el-tag v-bind:type="getGenderType(scope.row.gender)" size="large">
@@ -31,11 +26,19 @@
         </template>
       </el-table-column>
       <el-table-column prop="phone" label="电话"/>
+      <el-table-column prop="roleId" label="角色">
+        <template v-slot="scope">
+          <el-tag v-bind:type="getRoleType(scope.row.roleId)" size="large">
+            {{ getRoleMsg(scope.row.roleId) }}
+          </el-tag>
+        </template>
+      </el-table-column>
       <el-table-column prop="operation" label="操作">
         <el-button type="primary" size="default">编辑</el-button>
         <el-button type="danger" size="default">删除</el-button>
       </el-table-column>
     </el-table>
+    <!--    分页标签-->
     <el-pagination
         v-model:current-page="userData.pageNum"
         v-model:page-size="userData.pageSize"
@@ -46,15 +49,67 @@
         @current-change="handleCurrentChange"
         style="margin-top: 5px"
     />
+    <!--    对话框标签-->
+    <el-dialog v-model="dialogFormVisible" title="添加用户" width="500px" @close="resetForm()">
+      <el-form v-bind:model="user" ref="userForm">
+        <el-form-item prop="username" label="账号" style="width: 300px">
+          <el-input v-model="user.username" placeholder="请输入账号"/>
+        </el-form-item>
+        <el-form-item prop="nickname" label="姓名" style="width: 300px">
+          <el-input v-model="user.nickname" placeholder="请输入姓名"/>
+        </el-form-item>
+        <el-form-item prop="password" label="密码" style="width: 300px">
+          <el-input v-model="user.password" placeholder="请输入密码" show-password/>
+        </el-form-item>
+        <el-form-item prop="age" label="年龄" style="width: 300px">
+          <el-input v-model="user.age" placeholder="请输入年龄"/>
+        </el-form-item>
+        <el-form-item prop="gender" label="性别" style="width: 300px">
+          <el-select v-model="user.gender" placeholder="请选择性别">
+            <el-option label="男" value="男"/>
+            <el-option label="女" value="女"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="phone" label="电话" style="width: 300px">
+          <el-input v-model="user.phone" placeholder="请输入电话"/>
+        </el-form-item>
+        <el-form-item prop="role" label="角色" style="width: 300px">
+          <el-select v-model="user.roleId" placeholder="请选择角色">
+            <el-option label="超级管理员" value="0"/>
+            <el-option label="管理员" value="1"/>
+            <el-option label="普通用户" value="2"/>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="success" @click="doAdd()">确认</el-button>
+      </template>
+    </el-dialog>
   </el-scrollbar>
 </template>
 
 <script lang="ts" setup>
-import {onBeforeMount, reactive, ref} from 'vue'
+import {nextTick, onBeforeMount, reactive, ref} from 'vue'
 import {Search} from '@element-plus/icons-vue'
 import axios from "axios"
+import {ElMessage} from "element-plus";
 
 const baseURL = "http://localhost:8090/user"
+
+let dialogFormVisible = ref(false)//处理对话框是否显示
+
+const userForm = ref()//绑定form表单
+
+let user = reactive({
+  username: "",
+  nickname: "",
+  password: "",
+  age: null,
+  gender: "",
+  phone: "",
+  roleId: null
+})
 
 let userData = reactive({
   pageSize: 10,
@@ -69,9 +124,10 @@ let fuzzy = reactive({
   roleId: null
 })
 
-let keywords = ref("")//模糊查询的关键字
-
-let gender = ref("")//根据性别查询
+function resetPageData() {
+  userData.pageSize = 10
+  userData.pageNum = 1
+}
 
 function loadData() {//组件挂载之前从后端获取数据
   let promise = axios.get(baseURL, {
@@ -114,7 +170,6 @@ function fuzzyLoad() {//模糊查询
 onBeforeMount(() => {
   loadData()
 })
-
 
 function getRoleType(roleId) {//获取角色tag标签的type
   switch (roleId) {
@@ -162,9 +217,36 @@ function resetQuery() {
   fuzzy.gender = null
   fuzzy.roleId = null
   //重置分页数据
-  userData.pageNum = 1
-  userData.pageSize = 10
+  resetPageData()
   loadData()
+}
+
+function resetForm() {
+  if (userForm) {
+    nextTick(() => {//在DOM树更新完后执行
+      userForm.value.resetFields()
+    })
+  }
+}
+
+function doAdd() {
+  //将数据返回给后端
+  let promise = axios.post(baseURL, user);
+  promise.then(() => {
+    dialogFormVisible.value = false//关闭对话框
+    ElMessage({
+      message: "添加成功",
+      type: "success"
+    })
+    resetPageData()
+    loadData()
+  }).catch(error => {
+    ElMessage.error("添加失败")
+  })
+}
+
+function saveUser() {//添加用户
+  dialogFormVisible.value = true
 }
 </script>
 
