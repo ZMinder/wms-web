0791d501a11582a6f6889440b6da446b03682193
CMinder <minderctrl@gmail.com>
2024-05-31 20:19:46 +0800

    1.Aside侧边栏图标修改为使用Component动态获取

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -12,7 +12,7 @@
                 content="首页" v-bind:disabled="!collapse.isCollapse">
       <el-menu-item index="/home">
         <el-icon>
-          <House/>
+          <component :is="getIconComponent('House')"/>
         </el-icon>
         <span slot="title">首页</span>
       </el-menu-item>
@@ -21,8 +21,8 @@
                 effect="dark" v-bind:disabled="!collapse.isCollapse"
                 v-for="(item,index) in menu" v-bind:key="index">
       <el-menu-item v-bind:index=item.menuPath>
-        <el-icon >
-          <component :is="item.menuIcon"></component>
+        <el-icon>
+          <component :is="getIconComponent(item.menuIcon)"/>
         </el-icon>
         <span>{{ item.menuName }}</span>
       </el-menu-item>
@@ -31,7 +31,7 @@
 </template>
 
 <script lang="js" setup>
-import {Discount, House, Grid} from '@element-plus/icons-vue'
+import * as Icons from '@element-plus/icons-vue'
 import {asideCollapse} from '../store/store.ts'
 import {reactive} from "vue";
 
@@ -41,14 +41,19 @@ let menu = reactive([
   {
     menuName: "管理员管理",
     menuPath: "/admin",
-    menuIcon: Discount
+    menuIcon: "Discount"
   },
   {
     menuName: "用户管理",
     menuPath: "/user",
-    menuIcon: Grid
+    menuIcon: "Grid"
   }
 ])
+
+function getIconComponent(iconName) {//根据图标名称获取组件
+  console.log(Icons[iconName])
+  return Icons[iconName];
+}
 
 </script>
 
